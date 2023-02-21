a142d96169afbd351cba5d6c850389423eec71af
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-22 01:36:24 +0530

    country card component created and data rendered

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,17 +1,28 @@
-const CountryCard = ({ title }) => {
+const CountryCard = ({ country }) => {
+	const countryData = {
+		countryName: country.name.common,
+		countryPopulation: country.population,
+		countryInRegion: country.region,
+		countryFlag: country.flags.png,
+	};
+
+	// console.log(countryData);
+
 	return (
-		<div className="shadow-md rounded-lg cursor-pointer overflow-hidden bg-white dark:bg-slate-900 relative before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-blue-50 before:opacity-20">
-			<figure className="">
+		<div className="flex flex-col shadow-md rounded-lg cursor-pointer overflow-hidden bg-white dark:bg-slate-900 relative before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-blue-50 before:opacity-20">
+			<figure className="border h-[18rem]">
 				<img
-					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDoaDAtcvUA5T_4wv7Ef1c3BM9TKurWy6R2JVSRdXNvhTnhOHkSlsQgHd5crDen9yKAz8&usqp=CAU"
+					src={countryData.countryFlag}
 					alt="country flag"
-					className="w-full object-cover"
+					className="h-full w-full object-cover"
 				/>
 			</figure>
-			<div className="p-6 flex flex-col gap-4 text-[1.6rem]">
-				<h4 className="text-[2.4rem] font-semibold">{title}</h4>
-				<p>Population : 1.3B</p>
-				<p>Region : Asia</p>
+			<div className="p-8 flex flex-col gap-4 text-[1.6rem]">
+				<h4 className="text-[2.4rem] font-semibold mb-4">
+					{countryData.countryName}
+				</h4>
+				<p>{`Population : ${countryData.countryPopulation}`}</p>
+				<p>{`Region : ${countryData.countryInRegion}`}</p>
 				<p>Capital : Delhi</p>
 			</div>
 		</div>
