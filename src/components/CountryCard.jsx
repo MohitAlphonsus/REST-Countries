71ad18b87e7fced43c38e13ddeb4bb5f5328b07a
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-22 02:39:28 +0530

    required data extracted from country data

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,16 +1,25 @@
 const CountryCard = ({ country }) => {
 	const countryData = {
 		countryName: country.name.common,
-		countryPopulation: country.population,
+		countryPopulation: `${Number(country.population / 1000000).toFixed(2)}M`,
 		countryInRegion: country.region,
 		countryFlag: country.flags.png,
+		countryCapital: country.capital[0],
+
+		getCurrency() {
+			for (const curr in country.currencies) {
+				this.countryCurrency = country.currencies[curr];
+			}
+		},
 	};
 
-	// console.log(countryData);
+	countryData.getCurrency();
+
+	// console.log(country);
 
 	return (
 		<div className="flex flex-col shadow-md rounded-lg cursor-pointer overflow-hidden bg-white dark:bg-slate-900 relative before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-blue-50 before:opacity-20">
-			<figure className="border h-[18rem]">
+			<figure className="border-b dark:border-slate-800 h-[18rem]">
 				<img
 					src={countryData.countryFlag}
 					alt="country flag"
@@ -21,9 +30,18 @@ const CountryCard = ({ country }) => {
 				<h4 className="text-[2.4rem] font-semibold mb-4">
 					{countryData.countryName}
 				</h4>
-				<p>{`Population : ${countryData.countryPopulation}`}</p>
-				<p>{`Region : ${countryData.countryInRegion}`}</p>
-				<p>Capital : Delhi</p>
+				<p>
+					<strong>Population : </strong>
+					<span>{countryData.countryPopulation}</span>
+				</p>
+				<p>
+					<strong>Currency : </strong>
+					<span>{`${countryData.countryCurrency.name} ${countryData.countryCurrency.symbol}`}</span>
+				</p>
+				<p>
+					<strong>Capital : </strong>
+					<span>{countryData.countryCapital}</span>
+				</p>
 			</div>
 		</div>
 	);
